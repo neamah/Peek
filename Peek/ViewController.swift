9485b6fb75c86859e9b0a4520579892d69b52924
Neamah Hussein <neamahhs@gmail.com>
2015-08-17 22:20:32 -0700

    Initial Commit

---
+++ Peek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peek
+//
+//  Created by Neamah Hussein on 8/17/15.
+//  Copyright (c) 2015 Neamah Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
